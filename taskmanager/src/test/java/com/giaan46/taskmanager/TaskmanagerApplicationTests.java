9d7cc6f182f2c9c3ca47f883b2ac45879b866011
Giaan46 <gianlucamaida996@gmail.com>
2025-10-06 19:00:49 +0200

    primer commit

---
+++ taskmanager/src/test/java/com/giaan46/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giaan46.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
