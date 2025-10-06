c3e06cb5c107978f37d983879ba2ae6b25755b0e
Giaan46 <gianlucamaida996@gmail.com>
2025-10-06 19:07:07 +0200

    comentarios de prueba

--- taskmanager/src/main/java/com/giaan46/taskmanager/TaskmanagerApplication.java
+++ taskmanager/src/main/java/com/giaan46/taskmanager/TaskmanagerApplication.java
@@ -3,6 +3,7 @@ package com.giaan46.taskmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// esta es la clase aplication 
 @SpringBootApplication
 public class TaskmanagerApplication {
 
